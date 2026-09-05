3b1d35f97ddf1453f718d5b057b792b72d439be0
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-05 19:11:35 +0530

    Add Arithmetic_oper.java to demonstrate basic arithmetic operations in Java

---
+++ Java-Basics/Arithmetic_oper.java
@@ -0,0 +1,15 @@
+public class Arithmetic_oper {
+
+        public static void main(String args[]){
+                int A = 10;
+                int B = 20;
+                System.out.println("sum = " + (A+B));
+                System.out.println(A-B);
+                System.out.println(A*B);
+                System.out.println(A/B);
+                System.out.println(A%B);
+        }
+}
+
+    
+
