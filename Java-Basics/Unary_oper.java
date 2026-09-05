6c0e4f4695fa4c24a3284f324fb4a7fb02f96199
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-05 19:12:33 +0530

    Add Unary_oper.java to demonstrate unary operations in Java

---
+++ Java-Basics/Unary_oper.java
@@ -0,0 +1,25 @@
+public class Unary_oper {
+
+    public static void main(String args[]){
+                int a = 10;
+                int b = ++a;
+                System.out.println(a);
+                System.out.println(b);
+                
+                int a2 = 10;
+                int b2 = a2++;
+                System.out.println(a2);
+                System.out.println(b2);
+
+                // int x = 10;
+                // int y = --x;
+                // System.out.println(x);
+                // System.out.println(y);
+
+                int x2 = 10;
+                int y2 = x2--;
+                System.out.println(x2);
+                System.out.println(y2);
+        }
+    
+}
