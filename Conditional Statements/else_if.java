c0309016ca6714ee33b5826b01ab30339323e405
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-05 19:30:22 +0530

    Add else_if.java to demonstrate conditional statements using if-else structure in Java

---
+++ Conditional Statements/else_if.java
@@ -0,0 +1,18 @@
+public class else_if {
+
+     public static void main(String args[]){
+               int age = 12;
+
+               if(age>=18){
+                System.out.println("Adult");
+               }
+               else if(age>=13 && age<18){
+                System.out.println("Teenagers");
+               }
+               else{
+                System.out.println("Not Adult");
+               }
+               
+        }
+    
+}
