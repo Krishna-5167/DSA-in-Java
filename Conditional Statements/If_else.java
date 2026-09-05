9d27ea154837a5ae90ae62f72e2edce9c6bd1260
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-05 19:22:13 +0530

    Add If_else.java to demonstrate conditional statements in Java

---
+++ Conditional Statements/If_else.java
@@ -0,0 +1,21 @@
+public class If_else {
+    
+        public static void main(String args[]){
+               int age = 22;
+
+               if(age>=18){
+                System.out.println("Adult : vote , drive");
+               }
+
+               if(age>13 && age<18){
+                System.out.println("Teenagers");
+               }
+
+               else{
+                System.out.println("Not Adult");
+               }
+
+        }
+}
+
+
