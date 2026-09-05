a28167177774413fdc46795b803c3dcc36249fe7
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-05 19:30:31 +0530

    Add Even_Odd.java to demonstrate determining if a number is even or odd using conditional statements in Java

---
+++ Conditional Statements/Even_Odd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Even_Odd {
+        public static void main(String args[]){
+               Scanner sc = new Scanner(System.in);
+               int x = sc.nextInt();
+               if(x%2==0){
+                System.out.println("x is even");
+               } else{
+                System.out.println("x is odd");
+               }
+
+               sc.close();
+               
+        }
+}
+
+
