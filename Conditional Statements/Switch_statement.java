c7978efd5fdfddc6ea654efae81af9f2a14fa0d0
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-06 13:31:25 +0530

    Add Switch_statement.java to demonstrate the use of switch-case for conditional execution in Java

---
+++ Conditional Statements/Switch_statement.java
@@ -0,0 +1,18 @@
+public class Switch_statement {
+
+    public static void main(String args[]){
+               int number = 2;
+
+               switch(number){
+                case 1 : System.out.println("samosa");
+                                break;
+                case 2 : System.out.println("mango shake");
+                                break;
+                case 3 : System.out.println("juice");
+                                break;
+                default : System.out.println("wake up");
+               }
+               
+        }
+    
+}
