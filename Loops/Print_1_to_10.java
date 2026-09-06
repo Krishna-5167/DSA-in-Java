4dad3b6cc94852ed14c98be31a3f8632778cb10e
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-06 13:45:52 +0530

    Add Print_1_to_10.java to demonstrate printing numbers from 1 to 10 using a while loop in Java

---
+++ Loops/Print_1_to_10.java
@@ -0,0 +1,12 @@
+public class Print_1_to_10 {
+
+    public static void main(String args[]){
+        int i = 1;
+        while(i<11){
+            System.out.print(i + " ");
+            i++;
+        }
+        
+    }
+    
+}
