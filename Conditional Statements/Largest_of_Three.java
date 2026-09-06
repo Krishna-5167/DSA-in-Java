eb4af06b77dc574f9a4ae947883cfacbc2616d49
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-06 13:31:04 +0530

    Add Largest_of_Three.java to demonstrate finding the largest of three integers using conditional statements in Java

---
+++ Conditional Statements/Largest_of_Three.java
@@ -0,0 +1,17 @@
+public class Largest_of_Three {
+
+     public static void main(String args[]){
+               int a=1 , b = 12 , c = 3;
+               if(a>=b && a>=c){
+                System.out.println("a is the largest");
+               }
+               else if(b>=c){
+                System.out.println("b is the largest");
+               }
+               else{
+                System.out.println("c is the largest");
+               }
+               
+        }
+    
+}
