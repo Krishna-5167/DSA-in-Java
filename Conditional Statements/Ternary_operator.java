83b4a2ff9c0791ac9f3d3e337e5c79ddd96fd9cf
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-06 13:31:40 +0530

    Add Ternary_operator.java to demonstrate the use of the ternary operator for conditional evaluation in Java

---
+++ Conditional Statements/Ternary_operator.java
@@ -0,0 +1,12 @@
+public class Ternary_operator {
+
+     public static void main(String args[]){
+               int x = 5;
+
+               //ternary operator
+               String type = (x%2 == 0)?"even":"odd";
+               System.out.println(type);
+               
+        }
+    
+}
