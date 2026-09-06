68e7dcc49462da18e9c5811304adb9a62b7eea4a
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-06 13:45:37 +0530

    Add while_loop.java to demonstrate the use of a while loop for repetitive execution in Java

---
+++ Loops/while_loop.java
@@ -0,0 +1,13 @@
+public class while_loop {
+
+    public static void main(String args[]){
+        int counter = 0;
+        while(counter<10){
+            System.out.println("hello");
+            counter++;
+        }
+        System.out.println("Printed 10x times");
+        
+    }
+    
+}
