2fcb197c7f587e39e9b7b872b9bb4e0d21071389
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-07 09:51:05 +0530

    Refactor continue_Question.java to use try-with-resources for Scanner initialization

--- Loops/continue_Question.java
+++ Loops/continue_Question.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class continue_Question {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-
-        do{
+        try(Scanner sc = new Scanner(System.in)){
+            
+            do{
             System.out.print("Enter a number: ");
             int n = sc.nextInt();
             if(n%10 == 0){
@@ -13,6 +13,9 @@ public class continue_Question {
             System.out.println(n);
         }while(true);
 
+        }
+
+        
         
     }
     
