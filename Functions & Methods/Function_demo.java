64b3d313afbc2df4841ded36a4bee991ac8aedc5
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-08 09:19:53 +0530

    Add Function_demo.java to demonstrate a simple function that prints "Hello"

---
+++ Functions & Methods/Function_demo.java
@@ -0,0 +1,11 @@
+public class Function_demo {
+    public static void printHello(){
+        System.out.println("Hello");
+        
+    }
+    public static void main(String args[]){
+        printHello();
+
+    }  
+    
+}
