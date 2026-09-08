01ea73ca4bd2d301c80e333ea6f7dafcbfa0d754
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-08 09:20:45 +0530

    Add Prime_or_not.java to implement a method for checking prime numbers

---
+++ Functions & Methods/Prime_or_not.java
@@ -0,0 +1,25 @@
+public class Prime_or_not {
+    public static boolean isPrime(int n){
+
+        if(n == 2){
+            return true;
+        }
+         
+        for(int i = 2 ; i<= n-1 ; i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+    public static void main(String args[]){
+      
+        System.out.println(isPrime(5));
+        System.out.println(isPrime(9));
+        
+       
+    }
+    
+}
