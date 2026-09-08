61e8feb89f30270785eacf3aabb8642e4fffc600
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-08 09:20:10 +0530

    Add Product_a_And_b.java to implement a method for calculating the product of two integers

---
+++ Functions & Methods/Product_a_And_b.java
@@ -0,0 +1,14 @@
+public class Product_a_And_b {
+    public static int product(int a , int b){
+        int c = a*b;
+        return c;
+
+    }
+    public static void main(String args[]){
+       int prod = product(5 , 5);
+       System.out.println(prod);
+        
+       
+    }
+    
+}
