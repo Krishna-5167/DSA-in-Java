3be9f2f24156e4cb16c691be70e93a817aaea87b
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-08 09:21:43 +0530

    Add CheckPrime_Optimized.java to implement an optimized method for checking prime numbers

---
+++ Functions & Methods/CheckPrime_Optimized.java
@@ -0,0 +1,25 @@
+public class CheckPrime_Optimized {
+    public static boolean isPrime(int n){
+
+        if(n == 2){
+            return true;
+        }
+         
+        for(int i = 2 ; i<= Math.sqrt(n) ; i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+    public static void main(String args[]){
+      
+        System.out.println(isPrime(5));
+        System.out.println(isPrime(9));
+        
+       
+    }
+    
+}
