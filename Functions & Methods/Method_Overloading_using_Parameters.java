884d0f7c1af2c7b032e017a177987c70256f5ac8
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-08 09:21:50 +0530

    Add Method_Overloading_using_Parameters.java to demonstrate method overloading with sum calculations

---
+++ Functions & Methods/Method_Overloading_using_Parameters.java
@@ -0,0 +1,19 @@
+public class Method_Overloading_using_Parameters {
+    public static int sum(int a , int b){
+        return a+b;
+        
+    }
+
+    public static int sum(int a , int b, int c){
+        return a+b+c;
+        
+    }
+    public static void main(String args[]){
+      
+        System.out.println(sum(4 , 4));
+        System.out.println(sum(7,8,9));
+        
+       
+    }
+    
+}
