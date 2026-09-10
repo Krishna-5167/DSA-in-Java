0dc1db9dfb30c211255956f0584202c6a6c2c246
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-10 17:32:18 +0530

    Add Linear_Search.java to implement a method for linear search in an array

---
+++ Array-Basics/Linear_Search.java
@@ -0,0 +1,27 @@
+public class Linear_Search {
+    public static int linearSearch(int marks[] , int key){
+        for(int i = 0; i<marks.length ; i++){
+            if(marks[i] == key){
+                return i;
+            }
+        }
+        return -1;
+
+
+    }
+    public static void main(String args[]){
+        
+        
+        int marks[] = {2 , 4, 6, 8, 10 , 12 , 14 , 16};
+        int key = 12;
+        
+        int index = linearSearch(marks, key);
+        if(index == -1){
+            System.out.println("Not Found");
+        }else{
+            System.out.println("key is at index: "+ index);
+        }
+
+        
+    }
+}
