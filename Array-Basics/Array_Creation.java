f436a5ace7f87d7e2e54ec79e7f57fcf07f6db1b
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-10 17:31:05 +0530

    Add Array_Creation.java to implement basic array creation and initialization

---
+++ Array-Basics/Array_Creation.java
@@ -0,0 +1,13 @@
+public class Array_Creation {
+    public static void main(String args[]){
+        int marks[] = new int[50];
+
+        int numbers[] = {1 , 2 , 3 , 4 , 5};
+
+        String fruits[]= {"mango" , "apple" , "orange"};
+
+        System.out.println(marks.length);
+        System.out.println(numbers[0]);
+        System.out.println(fruits[0]);
+    }    
+}
