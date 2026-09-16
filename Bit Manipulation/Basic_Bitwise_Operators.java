d0b567cba0dbe6a6cd21d0fda72def7d6d087b39
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-16 21:51:52 +0530

    Add implementation of basic bitwise operators in Java

---
+++ Bit Manipulation/Basic_Bitwise_Operators.java
@@ -0,0 +1,11 @@
+public class Basic_Bitwise_Operators {
+    public static void main(String args[]) {
+        System.out.println((5 & 6));
+        System.out.println((5 | 6));
+        System.out.println((5 ^ 6));
+        System.out.println((~10));
+        System.out.println((5 << 2));
+        System.out.println((6 >> 1));
+    }
+    
+}
