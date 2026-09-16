eeafd2846e9b3b37c6edf131a8c0d59f31e2ba5e
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-16 21:54:19 +0530

    Add implementation to get the ith bit of a number using bitwise operations

---
+++ Bit Manipulation/Git_iTH_bit.java
@@ -0,0 +1,18 @@
+public class Git_iTH_bit {
+    public static int getIthBit(int n , int i){
+        int bitmask = 1<<i;
+        if((n & bitmask) == 0){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println(getIthBit(10 , 2));
+        System.out.println(getIthBit(10, 3));
+        
+    }
+    
+}
