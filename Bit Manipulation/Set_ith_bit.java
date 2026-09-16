2455c89645d725caa4e41871c60b972e323d99ce
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-16 21:55:21 +0530

    Add implementation to set the ith bit of a number using bitwise operations

---
+++ Bit Manipulation/Set_ith_bit.java
@@ -0,0 +1,12 @@
+public class Set_ith_bit {
+    public static int setIthBit(int n , int i){
+        int bitmask = 1<<i;
+        return n|bitmask;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(setIthBit(10 , 2));
+        
+    }
+    
+}
