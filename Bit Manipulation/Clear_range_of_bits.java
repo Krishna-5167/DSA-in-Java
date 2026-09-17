99ce2c136e62bdf5d44b2f431c16ef21beb3c156
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-17 20:01:08 +0530

    Add implementation to clear bits in a specified range of a number using bitwise operations

---
+++ Bit Manipulation/Clear_range_of_bits.java
@@ -0,0 +1,14 @@
+public class Clear_range_of_bits {
+    public static int clearBitsInRange(int n , int i , int j){
+        int a = ((~0)<<(j+1));
+        int b = (1<<i)-1;
+        int bitmask = a|b;
+        return n & bitmask;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(clearBitsInRange(10 , 2, 4));
+        
+    }
+    
+}
