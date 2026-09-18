6401fb8a0b269628e5a001fe53771864e2701534
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-18 20:48:35 +0530

    Add implementation to add 1 to an integer using bit manipulation

---
+++ Bit Manipulation/Add_1_to_an_integer_using_bitManipulation.java
@@ -0,0 +1,9 @@
+public class Add_1_to_an_integer_using_bitManipulation {
+    public static void main(String args[]) {
+        int x=6;
+        System.out.println(x + " + "+1 +" is "+ ~-x);
+        
+        int y=-4;
+        System.out.println(y + " + " +1 +" is "+ ~-y);
+    }
+}
