ba69fd2cf8a719df0f39a39dc75392170b3eb547
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-19 22:31:55 +0530

    Add implementation of access modifiers with bankAccount class example

---
+++ OOPs/Access_Modifiers.java
@@ -0,0 +1,23 @@
+class bankAccount{
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd){
+        password = pwd;
+    }
+
+
+}
+
+public class Access_Modifiers {
+    public static void main(String[] args) {
+
+        bankAccount myAcc = new bankAccount();
+
+        myAcc.username = "Krishna";
+        // myAcc.setPassword = "abcdef";        // This line is incorrect. It should be myAcc.setPassword("abcdef");
+
+        myAcc.setPassword("abcdef");
+
+    }
+}
