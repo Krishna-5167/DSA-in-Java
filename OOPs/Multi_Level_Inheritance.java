3c4b7e8a1cb81b64c30c033f20e066e47a0b549d
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-22 18:20:06 +0530

    Add implementation of Multi-Level Inheritance with Animal, Mammal, and Dog classes

---
+++ OOPs/Multi_Level_Inheritance.java
@@ -0,0 +1,37 @@
+class Animal{
+    String color;
+
+    void eat(){
+        System.out.println("Eats");
+    }
+
+    void breathe(){
+        System.out.println("Breathes");
+    }
+}
+
+class Mammal extends Animal{
+    int legs;
+
+}
+
+class Dog extends Mammal{
+    String breed;
+}
+
+
+
+
+public class Multi_Level_Inheritance {
+
+    public static void main(String[] args) {
+        Dog dobby = new Dog();
+        dobby.eat();
+        dobby.legs = 4;
+        System.out.println(dobby.legs);
+        
+        
+    }
+
+    
+}
