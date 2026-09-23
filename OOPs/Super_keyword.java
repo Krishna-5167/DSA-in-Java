d5c7a8873b16b54a9c95d690c02d9d43c0481ce6
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-23 22:16:14 +0530

    Add implementation of Animal and Horse classes demonstrating the use of the super keyword

---
+++ OOPs/Super_keyword.java
@@ -0,0 +1,30 @@
+class Animal{
+    String color;
+
+    Animal(){
+        System.out.println("Animal constructor is called");
+    }
+}
+
+class Horse extends Animal{
+    Horse(){
+
+    super.color = "brown";
+    System.out.println("Horse constructor is called");
+    
+    }
+}
+
+
+
+
+
+
+public class Super_keyword {
+
+    public static void main(String[] args) {
+        Horse h = new Horse();
+        System.out.println(h.color);        
+    }
+    
+}
