595ec66e79c59aa841c686ef01bafb0da816b279
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-24 22:07:40 +0530

    Print_numbers_in_decreasing_order class for recursive number printing

---
+++ Recursion1/Print_numbers_in_decreasing_order.java
@@ -0,0 +1,19 @@
+public class Print_numbers_in_decreasing_order {
+    
+
+    public static void printDec(int n){
+        if(n ==1){
+        System.out.print(n);
+        return;
+        }
+
+        System.out.print(n +" ");
+        printDec(n-1);
+    }
+
+    public static void main(String args[]){
+        int n = 10;
+        printDec(n);
+    }
+}
+
