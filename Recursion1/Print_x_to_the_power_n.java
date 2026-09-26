f37ebf87d9560e3dc4131c050db888e0f6b281bf
Krishna-5167 <krishnagoyal5167@gmail.com>
2026-09-26 20:09:52 +0530

    Add implementation of Print_x_to_the_power_n class with recursive method to calculate x to the power of n

---
+++ Recursion1/Print_x_to_the_power_n.java
@@ -0,0 +1,26 @@
+public class Print_x_to_the_power_n {
+
+    public static int power(int x , int n){
+        
+        if(n == 0){
+            return 1;
+        }
+
+        // int xnm1 = power(x ,n-1);
+        // int xn = x *xnm1;
+        // return xn;
+
+
+        // or
+        
+        return x * power(x , n-1);
+        
+        
+    }
+
+    public static void main(String args[]){
+        
+        System.out.println(power(2, 10));
+    }
+}
+
